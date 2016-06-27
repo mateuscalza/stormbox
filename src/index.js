34a87d0426a12c3ff4194e9788c38db3b71f827e
Mateus Calza <mateushenrique17@gmail.com>
2016-06-27 16:53:11 -0300

    Issue: refs #11026; Description: wrapper, icon and present element.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log('Testing...');
+/**
+ * ES7 AutoComplete
+ *
+ * @author Jackson Veroneze <jackson@inovadora.com.br>
+ * @author Ladislau Perrony <ladislau.perrony@inovadora.com.br>
+ * @author Mario Mendonça <mario@inovadora.com.br>
+ * @author Mateus Calza <mateus@inovadora.com.br>
+ * @author Patrick Nascimento <patrick@inovadora.com.br>
+ * @license MIT
+ * @version 1.0.0
+ */
+
+import 'babel-polyfill';
+import AutoComplete from './components/AutoComplete';
+
+import AjaxSource from './sources/AjaxSource';
+import SelectSource from './sources/SelectSource';
+import ArraySource from './sources/ArraySource';
+
+const autocomplete = options => new AutoComplete(options);
+autocomplete.ajax = url => new AjaxSource(url);
+autocomplete.select = select => new SelectSource(select);
+autocomplete.array = array => new ArraySource(array);
+autocomplete.byId = id => document.getElementById(id);
+
+if(typeof window !== 'undefined') {
+    window.autocomplete = autocomplete;
+}
+
+export default autocomplete;
--- src/util/dom.js
+++ src/util/dom.js
@@ -1,39 +1,44 @@
 export function elem(tag, props, ...children) {
-  var domElem = document.createElement(tag);
-  Object.assign(domElem, props);
-  children.forEach(child => {
-    domElem.appendChild(child);
-  });
-  domElem.on = function(eventName, callback) {
-    return domElem.addEventListener(eventName, callback);
-  };
-  return domElem;
+    var domElem = document.createElement(tag);
+    Object.assign(domElem, props);
+    children.forEach(child => {
+        domElem.appendChild(child);
+    });
+    domElem.on = function(eventName, callback) {
+        console.log(domElem, eventName, callback);
+        return domElem.addEventListener(eventName, callback);
+    };
+    return domElem;
 };
 
 export function input(props) {
-  return elem('input', props);
+    return elem('input', props);
 };
 
 export function div(props) {
-  return elem('div', props);
+    return elem('div', props);
 };
 
 export function ul(props) {
-  return elem('ul', props);
+    return elem('ul', props);
 };
 
 export function li(props) {
-  return elem('li', props);
+    return elem('li', props);
 };
 
 export function strong(props) {
-  return elem('strong', props);
+    return elem('strong', props);
 };
 
 export function a(props) {
-  return elem('a', props);
+    return elem('a', props);
+};
+
+export function i(props) {
+    return elem('i', props);
 };
 
 export function span(props) {
-  return elem('span', props);
+    return elem('span', props);
 };
